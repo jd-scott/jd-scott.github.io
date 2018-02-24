ea0d0bcea3dc1a2ab1b69b6431269003cea46514
jd scott <jdscott1004@gmail.com>
2018-02-23 23:04:29 -0800

    changed a bunch... mostly tried to get html to parse txt file using php

---
+++ unit2.php
@@ -0,0 +1,35 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Unit 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/main.css" rel="stylesheet" />
+</head>
+<body>
+
+
+    <!--
+        Jeremy You're going to need to concatenate php and html
+        do this using . operator, it isn't needed to link all code but is at certain
+        currently unknown points.
+
+
+        -->
+    <!--<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">-->
+    <?php
+    $txt1 = "Learn PHP";
+    $txt2 = "W3Schools.com";
+    $x = 5;
+    $y = 4;
+
+    echo $txt1;
+    echo $txt2;
+    echo $x + $y;
+
+    ?>
+    <p>Hello again...</p>
+    <footer>
+        <script src="javascript/main.js"></script>
+    </footer>
+</body>
+</html>
