33010e3c8c14ac5db386e6a1af14c491b48991d1
jd scott <jdscott1004@gmail.com>
2018-02-25 00:54:24 -0800

    moved var i to inside for loops in slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -10,17 +10,21 @@ function currentDiv(n) {
 	showDivs(slideIndex = n);
 }
 
+function currentDiv(n) {
+	showDivs(slideIndex = n);
+}
+
 function showDivs(n) {
-	var iSlide;
+
 	var x = document.getElementsByClassName("mySlides");
 	var dots = document.getElementsByClassName("demo");
 	if (n > x.length) { slideIndex = 1 }
 	if (n < 1) { slideIndex = x.length }
-	for (iSlide = 0; j < iSlide.length; iSlide++) {
-		x[iSlide].style.display = "none";
+	for (var i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
 	}
-	for (iSlide = 0; iSlide < dots.length; iSlide++) {
-		dots[iSlide].className = dots[iSlide].className.replace(" w3-red", "");
+	for (var i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" w3-red", "");
 	}
 	x[slideIndex - 1].style.display = "block";
 	dots[slideIndex - 1].className += " w3-red";
