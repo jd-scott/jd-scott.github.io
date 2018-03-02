5f89af5f3cd938fea4c839b6211884d6446af5a0
jd scott <jdscott1004@gmail.com>
2018-03-01 20:15:59 -0800

    used gify.com

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,31 +1,21 @@
-// JavaScript source code
-var slideIndex = 1;
-showDivs(slideIndex);
+// Slideshow
+var slideIndex = [1, 2];
+var slideId = ["mySlides1", "mySlides2","mySlides3"]
+showDivs(1, 0);
+showDivs(1, 1);
+showDivs(1, 2);
 
-function plusDivs(n) {
-	showDivs(slideIndex += n);
+function plusDivs(n, no) {
+	showDivs(slideIndex[no] += n, no);
 }
 
-function currentDiv(n) {
-	showDivs(slideIndex = n);
-}
-
-function currentDiv(n) {
-	showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-
-	var x = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("demo");
-	if (n > x.length) { slideIndex = 1 }
-	if (n < 1) { slideIndex = x.length }
-	for (var i = 0; i < x.length; i++) {
+function showDivs(n, no) {
+	var i;
+	var x = document.getElementsByClassName(slideId[no]);
+	if (n > x.length) { slideIndex[no] = 1 }
+	if (n < 1) { slideIndex[no] = x.length }
+	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
-	for (var i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" w3-red", "");
-	}
-	x[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " w3-red";
+	x[slideIndex[no] - 1].style.display = "block";
 }
